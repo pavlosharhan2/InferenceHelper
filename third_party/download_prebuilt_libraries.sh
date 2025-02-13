14c7927e59aeb2466a7c9cadd5d175e27dfa4fbd
Pavlo Sharhan <pavel.shargan@gmail.com>
2025-02-13 06:25:44 -0500

    depemdency fix

--- third_party/download_prebuilt_libraries.sh
+++ third_party/download_prebuilt_libraries.sh
@@ -101,8 +101,8 @@ git submodule update --init --recommend-shallow --depth 1
 # download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/mnn_prebuilt_linux_1804.tgz"
 # download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/mnn_prebuilt_linux_2004.tgz"
 # download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/mnn_prebuilt_windows.zip"
-download_and_extract "https://github.com/pavloshargan/InferenceHelper/releases/download/v1.0/tflite_prebuilt.zip"
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/tflite_prebuilt_linux_2004.tgz"
+# download_and_extract "https://github.com/pavloshargan/InferenceHelper/releases/download/v1.0/tflite_prebuilt.zip"
+download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/tflite_prebuilt_linux_2004.tgz"
 # download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/tflite_prebuilt_windows.zip"
 
 ### Download ncnn pre-built libraries from https://github.com/Tencent/ncnn ###
