681fbbc3e1ad6f71718474ad6bfa296b3905105e
pavlosharhan2 <95373134+pavlosharhan2@users.noreply.github.com>
2025-03-19 04:45:09 -0400

    moved prebuilt tflite into repository

--- third_party/download_prebuilt_libraries.sh
+++ third_party/download_prebuilt_libraries.sh
@@ -92,19 +92,6 @@ move_dir_to_shell_file
 ### Get other projects (dependencies) ###
 git submodule update --init --recommend-shallow --depth 1
 
-### Download pre-built libraries from https://github.com/iwatake2222/InferenceHelper_Binary ###
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/armnn_prebuilt_linux_1804.tgz"
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/armnn_prebuilt_linux_2004.tgz"
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/edgetpu_prebuilt_linux_1804.tgz"
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/edgetpu_prebuilt_linux_2004.tgz"
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/edgetpu_prebuilt_windows.zip"
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/mnn_prebuilt_linux_1804.tgz"
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/mnn_prebuilt_linux_2004.tgz"
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/mnn_prebuilt_windows.zip"
-# download_and_extract "https://github.com/pavloshargan/InferenceHelper/releases/download/v1.0/tflite_prebuilt.zip"
-download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/tflite_prebuilt_linux_2004.tgz"
-# download_and_extract "https://github.com/iwatake2222/InferenceHelper_Binary/releases/download/v20220210/tflite_prebuilt_windows.zip"
-
 ### Download ncnn pre-built libraries from https://github.com/Tencent/ncnn ###
 mkdir -p ncnn_prebuilt && cd ncnn_prebuilt
 download_and_extract_ncnn "android-vulkan"
@@ -114,7 +101,6 @@ download_and_extract_ncnn "ubuntu-2004"
 download_and_extract_ncnn "windows-vs2019-shared"
 download_and_extract_ncnn "windows-vs2019"
 cd ..
-
 
 ### Download NNabla pre-built libraries from https://nnabla.org/cpplib
 mkdir -p nnabla_prebuilt/windows-vs2019 && mkdir -p nnabla_prebuilt/aarch64 && cd nnabla_prebuilt
